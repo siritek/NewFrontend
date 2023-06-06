06ce75ac34ec65cd899192f65fe1b443ae62472a
Bhasker7924 <bhaskerreddy7924@gmail.com>
2023-06-06 23:58:34 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import Login from './Login';
 import App from './App';
-
+const root = ReactDOM.createRoot(document.getElementById('root')); 
 // Check if the user is logged in
 const isLoggedIn = false; // Replace with your authentication logic
 
-ReactDOM.render(
+root.render(
   isLoggedIn ? <App /> : <Login />,
-  document.getElementById('root')
+ 
 );
