03b3ffc03bc97e75e26583d41631b3d4ab04ccf3
Bhasker7924 <bhaskerreddy7924@gmail.com>
2023-09-15 01:38:00 +0530

    synopsis

--- src/Pages/Synopsis.js
+++ src/Pages/Synopsis.js
@@ -44,8 +44,19 @@ function Synopsis({ claimNumber }) {
       const response = await fetch(`http://localhost:8080/download-csv?claimNumber=${claimNumber}`);
       if (response.ok) {
         const blob = await response.blob();
-        saveAs(blob, 'claim_data.csv');
-        alert('CSV downloaded successfully!');
+        const currentDate = new Date();
+      const year = currentDate.getFullYear();
+      const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+      const day = String(currentDate.getDate()).padStart(2, '0');
+      const hours = String(currentDate.getHours()).padStart(2, '0');
+      const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+      const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+
+      const timestamp = `NXT-${year}-${month}-${day} ${hours}-${minutes}-${seconds}`;
+      const filename = `${timestamp}.csv`;
+
+      saveAs(blob, filename);
+      alert('CSV downloaded successfully!');
       } else {
         console.error('Failed to download CSV');
       }
